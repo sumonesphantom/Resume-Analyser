e58356c33a4c799daf188d54510ae84ef45c40aa
someonesphantom <venkatin2sl@gmail.com>
2025-07-27 00:30:12 +0800

    added upload UI but removal of pdf is not working

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -16,13 +16,10 @@ export function meta({ }: Route.MetaArgs) {
 export default function Home() {
   const { auth} =usePuterStore();
     const navigate = useNavigate();
-
-
     useEffect(()=>{
-
-        if(auth.isAuthenticated) navigate('/auth?next=/');
-
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
     },[auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
